f0e4c1d54d4d68fec6b14b13eeec31b9c664d655
yeninurazizah <yeninurazizah14@gmail.com>
2017-03-06 19:47:36 +0700

    modul3

--- asp_mvc_2/Models/DB/DemoModel.Context.cs
+++ asp_mvc_2/Models/DB/DemoModel.Context.cs
@@ -30,8 +30,8 @@ namespace asp_mvc_2.Models.DB
         public virtual DbSet<SYSUserProfile> SYSUserProfiles { get; set; }
         public virtual DbSet<SYSUserRole> SYSUserRoles { get; set; }
 
-        public System.Data.Entity.DbSet<asp_mvc_2.Models.ViewModel.UserSignUpView> UserSignUpViews { get; set; }
+        /*public System.Data.Entity.DbSet<asp_mvc_2.Models.ViewModel.UserSignUpView> UserSignUpViews { get; set; }
 
-        public System.Data.Entity.DbSet<asp_mvc_2.Models.ViewModel.UserLoginView> UserLoginViews { get; set; }
+        public System.Data.Entity.DbSet<asp_mvc_2.Models.ViewModel.UserLoginView> UserLoginViews { get; set; }*/
     }
 }
